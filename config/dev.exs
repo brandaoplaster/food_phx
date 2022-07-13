e3ba4220cf7ffefd646cdbf1156b4711ff0b63c8
brandaoplaster <brandaoplaster@gmail.com>
2022-07-12 22:36:29 -0300

    chore: Configuring tailwindcss

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,15 @@ config :food_phx, FoodPhxWeb.Endpoint,
   secret_key_base: "dTLiynWdvrIzwxbthwYYuNdNLh6u9UFLWfjycTNX21uFV0VQ6uk/Q1txAe25TUED",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
