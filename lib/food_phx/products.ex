11dab86ba1abd387c18237e1e112451191e39be3
brandaoplaster <brandaoplaster@gmail.com>
2022-07-26 23:15:30 -0300

    feat: Adding validation in the product form

--- lib/food_phx/products.ex
+++ lib/food_phx/products.ex
@@ -12,5 +12,6 @@ defmodule FoodPhx.Products do
     |> Repo.insert()
   end
 
-  def changeset_product, do: Product.changeset
+  def changeset_product(product, params), do: Product.changeset(product, params)
+  def changeset_product, do: Product.changeset()
 end
--- lib/food_phx_web/live/admin/products/form/form.ex
+++ lib/food_phx_web/live/admin/products/form/form.ex
@@ -2,6 +2,7 @@ defmodule FoodPhxWeb.Admin.Products.Form do
   use FoodPhxWeb, :live_component
 
   alias FoodPhx.Products
+  alias FoodPhx.Products.Product
 
   def update(assigns, socket) do
     changeset = Products.changeset_product()
@@ -9,6 +10,16 @@ defmodule FoodPhxWeb.Admin.Products.Form do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(changeset: changeset)}
+     |> assign(changeset: changeset)
+     |> assign(product: %Product{})}
+  end
+
+  def handle_event("validate", %{"product" => params}, socket) do
+    changeset =
+      socket.assigns.product
+      |> Products.changeset_product(params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
   end
 end
