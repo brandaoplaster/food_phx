8534ca654e4e057a543462595e2fc5590675bf4a
brandaoplaster <brandaoplaster@gmail.com>
2022-07-29 23:27:12 -0300

    feat: Adding click events to the modal

--- lib/food_phx_web/live/live_helpers.ex
+++ lib/food_phx_web/live/live_helpers.ex
@@ -2,13 +2,35 @@ defmodule FoodPhxWeb.LiveHelpers do
   import Phoenix.LiveView
   import Phoenix.LiveView.Helpers
 
+  alias Phoenix.LiveView.JS
+
   def modal(assigns) do
+    assigns = assign_new(assigns, :return_to, fn -> nil end)
+
     ~H"""
-    <div class="phx-modal fade-in" data-role="modal">
-      <div class="phx-modal-content fade-in-scale">
+    <div id="modal" class="phx-modal fade-in" data-role="modal" data-role="modal" phx-remove={hide_modal()}>
+      <div
+        id="modal-content"
+        class="phx-modal-content fade-in-scale"
+        phx-click-away={JS.dispatch("click", to: "#close")}
+        phx-key="escape"
+      >
+      <%= if @return_to do %>
+        <%= live_patch("x", to: @return_to, id: "close", class: "phx-modal-close", phx_click: hide_modal()) %>
+      <% else %>
+        <a id="close" href="#" class="phx-close-modal" phx-click={hide_modal()}>
+          <small>x</small>
+        </a>
+      <% end %>
         <%= render_slot(@inner_block) %>
       </div>
     </div>
     """
+  end
+
+  defp hide_modal(js \\ %JS{}) do
+    js
+    |> JS.hide(to: "#modal", trasition: "fade-out")
+    |> JS.hide(to: "#modal-content", trasition: "fade-out-scale")
   end
 end
