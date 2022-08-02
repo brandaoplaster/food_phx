d030d21088dd74211e28127344fe71c0e946a8e1
brandaoplaster <brandaoplaster@gmail.com>
2022-08-01 23:41:54 -0300

    chore: Adjusting test loads modal

--- test/food_phx_web/live/admin/products/form/form_test.exs
+++ test/food_phx_web/live/admin/products/form/form_test.exs
@@ -5,11 +5,19 @@ defmodule FoodPhxWeb.Admin.Products.FormTest do
   test "load modal to insert product", %{conn: conn} do
     {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
 
+    open_modal(view)
+
     assert has_element?(view, "[data-role=modal]")
     assert has_element?(view, "[data-role=product-form]")
 
     assert view
-           |> form("#new_product", product: %{name: ""})
+           |> form("#new", product: %{name: ""})
            |> render_change() =~ "can&#39;t be blank"
+  end
+
+  defp open_modal(view) do
+    view
+    |> element("[data-role=add-new-product]", "New")
+    |> render_click()
   end
 end
