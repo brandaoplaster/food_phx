0fc54fe6c7eaaf52b50b7a92ecddce7ffc1aed11
brandaoplaster <brandaoplaster@gmail.com>
2022-08-02 23:30:02 -0300

    chore: Adding test form creating product

--- test/food_phx_web/live/admin/products/form/form_test.exs
+++ test/food_phx_web/live/admin/products/form/form_test.exs
@@ -15,6 +15,21 @@ defmodule FoodPhxWeb.Admin.Products.FormTest do
            |> render_change() =~ "can&#39;t be blank"
   end
 
+  test "given a product when submit the form return a message success", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
+
+    open_modal(view)
+
+    payload = %{name: "coxinha", description: "test", price: 10, size: "small"}
+    {:ok, _, html} =
+      view
+      |> form("#new", product: payload)
+      |> render_submit()
+      |> follow_redirect(conn, Routes.admin_product_path(conn, :index))
+
+    assert html =~ "Product has created"
+  end
+
   defp open_modal(view) do
     view
     |> element("[data-role=add-new-product]", "New")
