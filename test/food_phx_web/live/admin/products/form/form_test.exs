ae137f0dac131a80d3632bcfc8bb2c79c4fdd128
brandaoplaster <brandaoplaster@gmail.com>
2022-08-03 21:44:06 -0300

    chore: Adding product update test

--- test/food_phx_web/live/admin/products/form/form_test.exs
+++ test/food_phx_web/live/admin/products/form/form_test.exs
@@ -1,6 +1,7 @@
 defmodule FoodPhxWeb.Admin.Products.FormTest do
   use FoodPhxWeb.ConnCase
   import Phoenix.LiveViewTest
+  import FoodPhx.Factory
 
   test "load modal to insert product", %{conn: conn} do
     {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
@@ -21,6 +22,7 @@ defmodule FoodPhxWeb.Admin.Products.FormTest do
     open_modal(view)
 
     payload = %{name: "coxinha", description: "test", price: 10, size: "small"}
+
     {:ok, _, html} =
       view
       |> form("#new", product: payload)
@@ -28,6 +30,36 @@ defmodule FoodPhxWeb.Admin.Products.FormTest do
       |> follow_redirect(conn, Routes.admin_product_path(conn, :index))
 
     assert html =~ "Product has created"
+  end
+
+  test "given a product that has already exist when click to edit the open modal", %{conn: conn} do
+    product = insert(:product)
+
+    {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
+
+    assert view
+           |> element("[data-role=edit-product][data-id=#{product.id}]")
+           |> render_click()
+
+    assert view
+           |> has_element?("#modal")
+
+    assert_patch(view, Routes.admin_product_path(conn, :edit, product))
+
+    assert view
+    |> form("##{product.id}", product: %{name: nil})
+    |> render_change() =~ "can&#39;t be blank"
+
+    {:ok, view, html} =
+      view
+      |> form("##{product.id}", product: %{name: "hot dog"})
+      |> render_submit()
+      |> follow_redirect(conn, Routes.admin_product_path(conn, :index))
+
+    assert html =~ "Product updated"
+
+    assert view
+           |> has_element?("[data-role=product-name][data-id=#{product.id}]", "hot dog")
   end
 
   defp open_modal(view) do
