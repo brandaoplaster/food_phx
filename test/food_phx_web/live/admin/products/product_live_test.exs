0dc081765444435940c1916a42cffc4d535b574f
brandaoplaster <brandaoplaster@gmail.com>
2022-08-06 21:36:08 -0300

    chore: Adding tests to delete page

--- test/food_phx_web/live/admin/products/product_live_test.exs
+++ test/food_phx_web/live/admin/products/product_live_test.exs
@@ -21,4 +21,18 @@ defmodule FoodPhxWeb.Admin.ProductLiveTest do
     assert has_element?(view, "[data-role=product-size]", product.size)
     # assert has_element?(view, "[data-role=product-item][data-id=#{product.id}]")
   end
+
+  test "given a product that has already exist when click to delete then remove", %{conn: conn} do
+    product = insert(:product)
+
+    {:ok, view, _html} = live(conn, Routes.admin_product_path(conn, :index))
+
+    assert has_element?(view, "[data-role=delete][data-id=#{product.id}]", "Delete")
+
+    assert view
+           |> element("[data-role=delete][data-id=#{product.id}]", "Delete")
+           |> render_click()
+
+    refute has_element?(view, "[data-role=delete][data-id=#{product.id}]")
+  end
 end
