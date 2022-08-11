32bab6421ddf4ebf6d763444a9924345b5fbd99a
brandaoplaster <brandaoplaster@gmail.com>
2022-08-10 21:50:59 -0300

    chore: Adding test in page show

---
+++ test/food_phx_web/live/admin/products/show/show_test.exs
@@ -0,0 +1,16 @@
+defmodule FoodPhxWeb.Admin.Products.ShowTest do
+  use FoodPhxWeb.ConnCase
+  import Phoenix.LiveViewTest
+  import FoodPhx.Factory
+
+  test "load page", %{conn: conn} do
+    product = insert(:product)
+
+    {:ok, view, _html} = live(conn, Routes.admin_product_show_path(conn, :show, product.id))
+
+    assert has_element?(view, "[data-role=name]", product.name)
+    assert has_element?(view, "[data-role=size]", product.size)
+    assert has_element?(view, "[data-role=description]", product.description)
+    assert has_element?(view, "[data-role=price]", Money.to_string(product.price))
+  end
+end
