1c61efde3e8261a7e027b2132ca1362200d6a162
brandaoplaster <brandaoplaster@gmail.com>
2022-08-20 23:29:31 -0300

    chore: Adding live session tests

---
+++ test/food_phx_web/live/live_sessions/permissions_test.exs
@@ -0,0 +1,35 @@
+defmodule LiveSessions.PermissionsTest do
+  use FoodPhxWeb.ConnCase
+  import Phoenix.LiveViewTest
+  import FoodPhx.Factory
+
+  describe "success when is admin" do
+    setup :register_and_log_in_admin
+
+    test "when admin is corrent", %{conn: conn} do
+      product = insert(:product)
+      {:ok, view, _} = live(conn, Routes.admin_product_path(conn, :index))
+      assert has_element?(view, "[data-role=product-action][dta-id=#{product.id}]")
+    end
+  end
+
+  describe "when is not admin" do
+    setup :register_and_log_in_user
+
+    test "when admin is wrong", %{conn: conn} do
+      {:error, {:redirect, %{flash: %{"error" => message}, to: "/"}}} =
+        live(conn, Routes.admin_product_path(conn, :index))
+
+      assert message == "You don`t have permissions to access this page"
+    end
+  end
+
+  describe "when user is not logged in" do
+    test "without loggin", %{conn: conn} do
+      {:error, {:redirect, %{flash: %{"error" => message}, to: "/users/log_in"}}} =
+        live(conn, Routes.admin_product_path(conn, :index))
+
+      assert message == "You must log in to access this page."
+    end
+  end
+end
