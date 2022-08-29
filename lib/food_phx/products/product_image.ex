20b51bab7c280a95019bf6820621d84fd773296a
brandaoplaster <brandaoplaster@gmail.com>
2022-08-28 23:26:58 -0300

    chore: Adding logic to save image URL

--- lib/food_phx/products/product_image.ex
+++ lib/food_phx/products/product_image.ex
@@ -17,6 +17,6 @@ defmodule FoodPhx.Products.ProductImage do
   end
 
   def storage_dir(_, {_file, product}) do
-    "/uploads/products/#{product.id}"
+    "/priv/static/uploads/products/#{product.name}"
   end
 end
