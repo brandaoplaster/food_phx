ab24d744b2bbc60e603ab6c01745026c684c2e10
brandaoplaster <brandaoplaster@gmail.com>
2022-08-31 23:32:39 -0300

    chore: [WIP] show products

--- lib/food_phx_web/live/main/items/items.ex
+++ lib/food_phx_web/live/main/items/items.ex
@@ -2,4 +2,16 @@ defmodule FoodPhxWeb.Main.Items do
   use FoodPhxWeb, :live_component
 
   alias FoodPhxWeb.Main.Items.Item
+  alias FoodPhx.Products
+
+  def update(assigns, socket) do
+    products = Products.list_products()
+
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign(products: products)
+
+    {:ok, socket}
+  end
 end
