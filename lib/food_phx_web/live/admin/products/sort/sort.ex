bc5c7dad6ebdf20608ea1eea79290a7259492aba
brandaoplaster <brandaoplaster@gmail.com>
2022-09-11 21:23:55 -0300

    chore: Adding update function

--- lib/food_phx_web/live/admin/products/sort/sort.ex
+++ lib/food_phx_web/live/admin/products/sort/sort.ex
@@ -1,3 +1,26 @@
 defmodule FoodPhxWeb.Admin.Products.Sort do
   use FoodPhxWeb, :live_component
+
+  def update(assigns, socket) do
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign_sort_order(assigns)
+
+    {:ok, socket}
+  end
+
+  defp assign_sort_order(socket, %{options: %{sort_order: sort_order}}) do
+    case sort_order == :desc do
+      true ->
+        socket
+        |> assign(icon: "sort_desc.svg")
+        |> assign(sort_order: :asc)
+
+      false ->
+        socket
+        |> assign(icon: "sort_asc.svg")
+        |> assign(sort_order: :desc)
+    end
+  end
 end
