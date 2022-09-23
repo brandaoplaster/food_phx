a27562518148f74bc507036f3e8c5f0ca34f6624
brandaoplaster <brandaoplaster@gmail.com>
2022-09-22 22:20:01 -0300

    chore: Adding event update select per page

--- lib/food_phx_web/live/admin/products/paginate/paginate.ex
+++ lib/food_phx_web/live/admin/products/paginate/paginate.ex
@@ -21,4 +21,17 @@ defmodule FoodPhxWeb.Admin.Products.Paginate do
       ), "data-role": "paginate", "data-id": data_id) %>
     """
   end
+
+  def handle_event("update-select-perpage", %{"per-page-select" => per_page}, socket) do
+    to =
+      Routes.admin_product_path(socket, :index,
+        page: socket.assigns.option.page,
+        per_page: per_page,
+        sort_by: socket.assigns.option.sort_by,
+        sort_order: socket.assigns.option.sort_order,
+        name: socket.assigns.name
+      )
+
+    {:noreply, push_patch(socket, to: to)}
+  end
 end
