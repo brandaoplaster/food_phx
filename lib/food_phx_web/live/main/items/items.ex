0d080f076735091ec8f2cf954aa2489eaa685cf8
brandaoplaster <brandaoplaster@gmail.com>
2022-09-24 21:38:44 -0300

    feat: Adding event load more

--- lib/food_phx_web/live/main/items/items.ex
+++ lib/food_phx_web/live/main/items/items.ex
@@ -5,13 +5,29 @@ defmodule FoodPhxWeb.Main.Items do
   alias FoodPhxWeb.Main.Items.Item
 
   def update(assigns, socket) do
-    products = Products.list_products()
-
     socket =
       socket
       |> assign(assigns)
-      |> assign(products: products)
+      |> assign(page: 1, per_page: 8)
+      |> assigns_products()
 
     {:ok, socket}
+  end
+
+  def handle_event("load-more", _, socket) do
+    socket =
+      socket
+      |> update(:page, &(&1 + 1))
+      |> assigns_products()
+
+    {:noreply, socket}
+  end
+
+  defp assigns_products(socket) do
+    page = socket.assigns.page
+    per_page = socket.assigns.per_page
+    paginate = [{:paginate, %{page: page, per_page: per_page}}]
+    products = Products.list_products(paginate)
+    assign(socket, products: products)
   end
 end
