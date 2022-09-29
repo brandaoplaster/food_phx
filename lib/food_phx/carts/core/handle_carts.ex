9a6b9641c86623293b290f6d1e8beeb71a671cc9
brandaoplaster <brandaoplaster@gmail.com>
2022-09-28 21:28:21 -0300

    feat: Adding function to add item to cart

--- lib/food_phx/carts/core/handle_carts.ex
+++ lib/food_phx/carts/core/handle_carts.ex
@@ -2,4 +2,17 @@ defmodule FoodPhx.Carts.Core.HandleCarts do
   defstruct id: nil, items: [], total_items: 0, total_amount: 0, total_price: Money.new(0)
 
   def create_cart(id), do: %__MODULE__{id: id}
+
+  def add_new_cart(cart, item) do
+    total_price = Money.add(cart.total_price, item.price)
+    total_items = cart.items + 1
+
+    %{
+      cart
+      | total_amount: cart.total_amount + 1,
+        items: cart.items ++ [item],
+        total_price: total_price,
+        total_items: total_items
+    }
+  end
 end
--- test/food_phx/carts/core/handle_carts_test.exs
+++ test/food_phx/carts/core/handle_carts_test.exs
@@ -1,17 +1,32 @@
 defmodule FoodPhx.Carts.Core.HandleCartsTest do
   use FoodPhx.DataCase
 
+  import FoodPhx.Factory
+
   alias FoodPhx.Carts.Core.HandleCarts
+
+  @start_cart %HandleCarts{
+    id: 1,
+    items: [],
+    total_amount: 0,
+    total_items: 0,
+    total_price: %Money{amount: 0, currency: :BRL}
+  }
 
   describe "handle carts" do
     test "should create a new cart" do
-      assert %HandleCarts{
-               id: 1,
-               items: [],
-               total_amount: 0,
-               total_items: 0,
-               total_price: %Money{amount: 0, currency: :BRL}
-             } == HandleCarts.create_cart(1)
+      assert @start_cart == HandleCarts.create_cart(1)
+    end
+
+    test "should add a new item  in the car" do
+      product = insert(:produc)
+
+      cart = HandleCarts.add_new_cart(@start_cart, product)
+
+      assert 1 == cart.total_amount
+      assert 1 == cart.total_items
+      assert [product] == cart.items
+      assert product.price == cart.total_price
     end
   end
 end
