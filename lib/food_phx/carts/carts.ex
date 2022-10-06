9c689fb64e9b35dabcf7dd05957b1633514db7a3
brandaoplaster <brandaoplaster@gmail.com>
2022-10-05 22:51:38 -0300

    chore: Fixing remove function

--- lib/food_phx/carts/carts.ex
+++ lib/food_phx/carts/carts.ex
@@ -2,8 +2,8 @@ defmodule FoodPhx.Carts do
   def create(cart_id),
     do: GenServer.cast(:cart_session, {:create, cart_id})
 
-  def remove(cart_id),
-    do: GenServer.cast(:cart_session, {:remove, cart_id})
+  def remove(cart_id, product_id),
+    do: GenServer.call(:cart_session, {:remove, cart_id, product_id})
 
   def get(cart_id),
     do: GenServer.call(:cart_session, {:get, cart_id})
