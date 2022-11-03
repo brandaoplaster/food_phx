60d6c44ce26471ac73015f9774915c75646b8ff7
brandaoplaster <brandaoplaster@gmail.com>
2022-11-02 21:42:31 -0300

    chore: [WIP] in component layer

--- lib/food_phx_web/live/admin/orders/layer/layer.ex
+++ lib/food_phx_web/live/admin/orders/layer/layer.ex
@@ -1,3 +1,10 @@
 defmodule FoodPhxWeb.Admin.Orders.Layer do
   use FoodPhxWeb, :live_component
+
+  alias __MODULE__.Card
+
+  def update(assigns, socket) do
+    cards = [%{id: "1233"}]
+    {:ok, socket |> assign(assigns) |> assign(cards: cards)}
+  end
 end
