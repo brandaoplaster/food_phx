ff52a3c28efac9e020cd27b40880ba2966aa8507
brandaoplaster <brandaoplaster@gmail.com>
2022-11-30 21:45:08 -0300

    Feature/adding cards (#47)
    * test: Adding test to card
    
    * feat: Adding card component
    
    * chore: [WIP] in component layer
    
    * feat: Adding card layout

--- lib/food_phx_web/live/admin/orders/layer/layer.ex
+++ lib/food_phx_web/live/admin/orders/layer/layer.ex
@@ -1,3 +1,19 @@
 defmodule FoodPhxWeb.Admin.Orders.Layer do
   use FoodPhxWeb, :live_component
+
+  alias __MODULE__.Card
+
+  def update(assigns, socket) do
+    cards = [
+      %{id: "1233"},
+      updatea_at: DateTime.utc_now(),
+      status: :NOT_STARTED,
+      user: %{email: "l@gmail.com"},
+      total_price: Money.new(300),
+      total_amount: 2,
+      items: [%{id: "321", amount: 10, product: %{name: "queijo", price: Money.new(200)}}]
+    ]
+
+    {:ok, socket |> assign(assigns) |> assign(cards: cards)}
+  end
 end
