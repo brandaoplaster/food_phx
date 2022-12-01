e8846b5d318cf9a5f29e6ebcfc06665936e70cf7
brandaoplaster <brandaoplaster@gmail.com>
2022-11-30 21:43:39 -0300

    feat: Adding card layout

--- lib/food_phx_web/live/admin/orders/layer/layer.ex
+++ lib/food_phx_web/live/admin/orders/layer/layer.ex
@@ -4,7 +4,16 @@ defmodule FoodPhxWeb.Admin.Orders.Layer do
   alias __MODULE__.Card
 
   def update(assigns, socket) do
-    cards = [%{id: "1233"}]
+    cards = [
+      %{id: "1233"},
+      updatea_at: DateTime.utc_now(),
+      status: :NOT_STARTED,
+      user: %{email: "l@gmail.com"},
+      total_price: Money.new(300),
+      total_amount: 2,
+      items: [%{id: "321", amount: 10, product: %{name: "queijo", price: Money.new(200)}}]
+    ]
+
     {:ok, socket |> assign(assigns) |> assign(cards: cards)}
   end
 end
